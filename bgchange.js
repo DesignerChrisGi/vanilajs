cd48f8e2af4a577da41b64ff60dd106cbb048fb0
Chris <chris.gihoon@gmail.com>
2022-11-05 21:21:50 +0100

    Revert "init vanilajs clone on friday night, coding like a"
    This reverts commit ef156e3a5fa4dbae415d21598fb6aa12ab08c138.

--- bgchange.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector("#bgChange");
-
-function bgChange() {
-  const images = ["bg1.jpg", "bg2.jpg", "bg3.jpg"];
-  const chosenImage = images[Math.floor(Math.random() * images.length)];
-  document.body.style.backgroundImage = `url(${chosenImage})`;
-  document.body.style.backgroundSize = "fit";
-}
